5b42262f5a8ecc423b2bfa13220c5ea8cb1c3944
AMalfez <a_mansuri@ch.iitr.ac.in>
2024-06-15 16:25:28 +0530

    changed ui

--- components/bars/BottomBar.jsx
+++ components/bars/BottomBar.jsx
@@ -12,12 +12,12 @@ function BottomBar() {
   const path = usePathname();
   return (
     <div className="w-full text-white h-30 flex gap-1 p-2 lg:hidden">
-      <Link href={"/"} className={`${path==="/" ? "bg-blue-600":"hover:bg-neutral-700"} w-1/6 cursor-pointer flex items-center justify-center rounded p-5`}><MdHomeFilled className="text-lg p-0" /></Link>
-      <Link href={"/search"} className={`${path==="/search" ? "bg-blue-600":"hover:bg-neutral-700"} w-1/6 cursor-pointer rounded flex items-center justify-center p-5`}><CiSearch className="text-lg p-0" /></Link>
-      <Link href={"/activity"} className={`${path==="/activity" ? "bg-blue-600":"hover:bg-neutral-700"} w-1/6 cursor-pointer rounded flex items-center justify-center p-5`}><BsActivity className="text-lg p-0" /></Link>
-      <Link href={"/create"} className={`${path==="/create" ? "bg-blue-600":"hover:bg-neutral-700"} w-1/6 cursor-pointer rounded flex items-center justify-center p-5`}><IoIosCreate className="text-lg p-0" /></Link>
-      <Link href={"/community"} className={`${path==="/community" ? "bg-blue-600":"hover:bg-neutral-700"} w-1/6 cursor-pointer rounded flex items-center justify-center p-5`}><FaUserGroup className="text-md p-0" /></Link>
-      <Link href={"/profile"} className={`${path==="/profile" ? "bg-blue-600":"hover:bg-neutral-700"} w-1/6 cursor-pointer rounded flex items-center justify-center p-5`}><FaUser className="text-md p-0" /></Link>
+      <Link href={"/"} className={`${path==="/" ? "bg-blue-600":"hover:bg-neutral-700"} w-1/6 cursor-pointer flex items-center justify-center rounded p-2`}><MdHomeFilled className="text-lg p-0" /></Link>
+      <Link href={"/search"} className={`${path==="/search" ? "bg-blue-600":"hover:bg-neutral-700"} w-1/6 cursor-pointer rounded flex items-center justify-center p-2`}><CiSearch className="text-lg p-0" /></Link>
+      <Link href={"/activity"} className={`${path==="/activity" ? "bg-blue-600":"hover:bg-neutral-700"} w-1/6 cursor-pointer rounded flex items-center justify-center p-2`}><BsActivity className="text-lg p-0" /></Link>
+      <Link href={"/create"} className={`${path==="/create" ? "bg-blue-600":"hover:bg-neutral-700"} w-1/6 cursor-pointer rounded flex items-center justify-center p-2`}><IoIosCreate className="text-lg p-0" /></Link>
+      <Link href={"/community"} className={`${path==="/community" ? "bg-blue-600":"hover:bg-neutral-700"} w-1/6 cursor-pointer rounded flex items-center justify-center p-2`}><FaUserGroup className="text-md p-0" /></Link>
+      <Link href={"/profile"} className={`${path==="/profile" ? "bg-blue-600":"hover:bg-neutral-700"} w-1/6 cursor-pointer rounded flex items-center justify-center p-2`}><FaUser className="text-md p-0" /></Link>
     </div>
   )
 }
