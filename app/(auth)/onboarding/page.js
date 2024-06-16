960a20607b95a9375158837c17d54d02706f2226
AMalfez <a_mansuri@ch.iitr.ac.in>
2024-06-16 12:28:07 +0530

    initialized bd with prisma and done onboarding ui

--- app/(auth)/onboarding/page.js
+++ app/(auth)/onboarding/page.js
@@ -2,7 +2,7 @@ import AccountProfile from "@/components/account/AccountProfile";
 import {currentUser} from '@clerk/nextjs/server'
 async function Page() {
   const user = await currentUser()
-  console.log(user);
+  // console.log(user);
   const userInfo = {};
   const userData = {
     id:user.id,
@@ -13,7 +13,7 @@ async function Page() {
     image: userInfo.image ? userInfo.image : user.imageUrl
   }
   return (
-    <main className="border border-white h-screen p-14 lg:p-20">
+    <main className="h-fit flex flex-col justify-center items-center p-20">
         <h1 className="text-3xl font-medium">Onboarding</h1>
         <p>Complete your profile now to use Gappe</p>
         <AccountProfile userData={userData} btnTitle={"Continue"} />
